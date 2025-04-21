45e2908bbd81290042c12b3aefac1eccf53026a9
zerosugar6464 <musickumal@gmail.com>
2025-04-21 08:52:27 +0300

    Update Youtube.py

--- InflexMusic/platforms/Youtube.py
+++ InflexMusic/platforms/Youtube.py
@@ -1,15 +1,29 @@
+# Copyright (C) 2024 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
+# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
+
+""""
+TheTeamAlexa is a project of Telegram bots with variety of purposes.
+Copyright (c) 2024 -present Team=Alexa <https://github.com/TheTeamAlexa>
+
+This program is free software: you can redistribute it and can modify
+as you want or you can collabe if you have new ideas.
+"""
 import asyncio
 import os
 import re
+import json
 from typing import Union
 
-import yt_dlp
+from yt_dlp import YoutubeDL
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
+import config
 from InflexMusic.utils.database import is_on_off
 from InflexMusic.utils.formatters import time_to_seconds
+
+USE_COOKIES_ONLY = False
 
 
 def cookiefile():
